869c560d7f29d770a6fbce2559f4672868e8b9c3
EdwardShanahan07 <>
2023-04-23 11:43:42 +0000

    Refactor TestStudent class

--- test_student.py
+++ test_student.py
@@ -2,21 +2,29 @@ import unittest
 from student import Student
 
 class TestStudent(unittest.TestCase):
-    def test_full_name(self): 
-        student = Student("John", "Doe")
+    def setUp(self):
+        print("Setup")
+        self.student = Student("John", "Doe")
 
-        self.assertEqual(student.full_name, "John Doe")
+    
+    def tearDown(self):
+        print("Tear Down")
+
+
+    def test_full_name(self): 
+        print("Test full name")
+        self.assertEqual(self.student.full_name, "John Doe")
+
 
     def test_email_address(self):
-        student = Student("John", "Doe")
+        print("Test email address")
+        self.assertEqual(self.student.email_address, "john.doe@email.com")
 
-        self.assertEqual(student.email_address, "john.doe@email.com")
 
     def test_alert_santa(self):
-        student = Student("John", "Doe")
-        student.alert_santa()
-
-        self.assertTrue(student.naughty_list)
+        print("Test alert santq")
+        self.student.alert_santa()
+        self.assertTrue(self.student.naughty_list)
 
 
 if __name__ == "__main__":
